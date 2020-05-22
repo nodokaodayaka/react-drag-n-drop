8293b0b0b4f2d9401d6513eda01fbf61f6b312f9
nodokaodayaka <wankosob@gmail.com>
2020-05-22 13:47:57 +0900

    first commit

---
+++ public/main.js
@@ -0,0 +1,42 @@
+const list_items = document.querySelectorAll('.list-item');
+const lists = document.querySelectorAll('.list');
+
+let draggedItem = null;
+
+for (let i = 0; i < list_items.length; i++) {
+    const item = list_items[i];
+
+    item.addEventListener('dragstart', function (e) {
+        draggedItem = this;
+        setTimeout(function () {
+            draggedItem.style.display = 'none';
+        });
+    });
+
+    item.addEventListener('dragend', function (e) {
+        setTimeout(function () {
+            draggedItem.style.display = 'block';
+            draggedItem = null;
+        }, 0);
+    })
+
+    for (let j = 0; j < lists.length; j++) {
+        const list = lists[j];
+
+        list.addEventListener('dragover', function (e) {
+            e.preventDefault();
+        });
+        list.addEventListener('dragenter', function (e) {
+            e.preventDefault();
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+        });
+        list.addEventListener('dragleave', function (e) {
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+        })
+
+        list.addEventListener('drop', function (e) {
+            this.append(draggedItem);
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+        });
+    }
+}
